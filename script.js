f60efab8838ff57eadd35c6170665e6e8b623eab
Tamires Oliveira <71903197+tamiressoliveira@users.noreply.github.com>
2022-06-29 12:32:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+const relogio = setInterval(function time(){
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
-
-
-const relogio = setInterval(function time(){
+    
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
